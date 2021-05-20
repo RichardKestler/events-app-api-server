f58ba26d8e4cc64e2f1f0777baa8b189f307de6a
RichardKestler <rkestler@deloitte.com>
2021-05-20 16:37:51 +0000

    upper case team name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Kickers Soccer Team';
+    team = 'KICKERS Soccer Team';
     version = '1.0.1';
 }
 module.exports = AppConfig;
